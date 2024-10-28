1a9bf897108ec792b796c7cb9cf3a8c9fdb2dce4
FichikTushafa <jacobalvarez843@yahoo.com>
2024-10-27 20:18:53 -0400

    Update Script.R

--- Script.R
+++ Script.R
@@ -3,15 +3,6 @@ library(tidyverse)
 install.packages("terra")
 library(terra)
 f <- vect("tl_2023_us_aiannh.shp")
-plot(f, col = f$NAME == "Choctaw")
-df <- f |> data.frame
-df <-
-  df |>
-  filter(GEOIDFQ == "2520000US5590R")
-CN <- vect(df, geom=c("lon", "lat"), crs="", keepgeom=
-             TRUE)
 f <-
 subset(f, f$GEOIDFQ == "2520000US5590R")
-plot(
-  f
-)
+plot(f)
