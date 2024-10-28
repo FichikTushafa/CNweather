9553119cd1c8a70543fe07ced42976641eb02d9a
FichikTushafa <jacobalvarez843@yahoo.com>
2024-10-27 20:40:20 -0400

    Update Script.R

--- Script.R
+++ Script.R
@@ -2,7 +2,7 @@ library(tidyverse)
 ###terra
 install.packages("terra")
 library(terra)
-f <- vect("tl_2023_us_aiannh.shp")
+f <- vect("tl_2023_us_aiannh.shp") ##keep ALL the files in the zip or it breaks!
 f <-
 subset(f, f$GEOIDFQ == "2520000US5590R")
 plot(f)
