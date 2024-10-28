fde4670a556e69a262772af8e00c6e2cdbb8653c
FichikTushafa <jacobalvarez843@yahoo.com>
2024-10-27 21:08:12 -0400

    Update Script.R

--- Script.R
+++ Script.R
@@ -1,8 +1,16 @@
 library(tidyverse)
-###terra
-install.packages("terra")
 library(terra)
 f <- vect("tl_2023_us_aiannh.shp") ##keep ALL the files in the zip or it breaks!
-f <-
-subset(f, f$GEOIDFQ == "2520000US5590R")
+f <- subset(f, f$GEOIDFQ == "2520000US5590R")
 plot(f)
+OK <- vect("COUNTY_BOUNDARY.shp")
+library(maptiles)
+bg <- get_tiles(ext(OK))
+plotRGB(bg)
+lines(f, col="blue", lwd=3)
+df <- OK |> data.frame()
+
+
+
+OK <- subset(OK, OK$COUNTY_NAM == names)
+names <- c("ATOKA", "BRYAN", "CHOCTAW", "COAL", "HASKELL", "HUGHES", "JOHNSTON", "LATIMER", "LE FLORE", "MCCURTAIN", "PITTSBURG", "PONTOTOC", "PUSHMATAHA")
