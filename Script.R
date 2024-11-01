0a26f0e17d7a8517819d3bcc749ad97a9da13e51
FichikTushafa <jacobalvarez843@yahoo.com>
2024-11-01 18:24:20 -0400

    Update Script.R

--- Script.R
+++ Script.R
@@ -1,16 +1,22 @@
 library(tidyverse)
 library(terra)
-f <- vect("tl_2023_us_aiannh.shp") ##keep ALL the files in the zip or it breaks!
-f <- subset(f, f$GEOIDFQ == "2520000US5590R")
-plot(f)
-OK <- vect("COUNTY_BOUNDARY.shp")
+
+#create CN plot
+CN <- vect("tl_2023_us_aiannh.shp") ##keep ALL the files in the zip or it breaks!
+CN <- subset(CN, CN$GEOIDFQ == "2520000US5590R")
+
+#create OK counties plot
+Counties <- vect("COUNTY_BOUNDARY.shp")
+Counties <- subset(Counties, Counties$COUNTY_NO %in% c("3", "7", "12",
+                                     "15", "31", "32",
+                                     "35", "39", "40",
+                                     "45", "61", "62", "64")) #this might not actually be necessary
+
+#plot counties cropped to CN borders
+SEOK <- crop(Counties, CN)
+
+
 library(maptiles)
-bg <- get_tiles(ext(OK))
-plotRGB(bg)
-lines(f, col="blue", lwd=3)
-df <- OK |> data.frame()
-
-
-
-OK <- subset(OK, OK$COUNTY_NAM == names)
-names <- c("ATOKA", "BRYAN", "CHOCTAW", "COAL", "HASKELL", "HUGHES", "JOHNSTON", "LATIMER", "LE FLORE", "MCCURTAIN", "PITTSBURG", "PONTOTOC", "PUSHMATAHA")
+bg <- get_tiles(ext(CN), crop = TRUE, zoom = 10)
+plotRGB(crop(bg, CN))
+lines(SEOK, col="blue", lwd=3)
